9ea2c63a021a29df138a1ebec9e5f3921c73cec1
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2016-07-16 17:54:46 +0300

    make and start restygwt working

--- src/main/java/com/samodeika/client/application/ServiceFactory.java
+++ src/main/java/com/samodeika/client/application/ServiceFactory.java
@@ -2,10 +2,12 @@ package com.samodeika.client.application;
 
 import com.google.gwt.core.client.GWT;
 import com.google.inject.Inject;
-import com.samodeika.shared.service.HelloService;
 import com.samodeika.shared.service.HelloServiceAsync;
 import com.samodeika.shared.service.TaskService;
 import com.samodeika.shared.service.TaskServiceAsync;
+import org.fusesource.restygwt.client.Defaults;
+import org.fusesource.restygwt.client.Resource;
+import org.fusesource.restygwt.client.RestServiceProxy;
 
 public class ServiceFactory {
 
--- src/main/java/com/samodeika/server/service/HelloServiceImpl.java
+++ src/main/java/com/samodeika/server/service/HelloServiceImpl.java
@@ -3,11 +3,15 @@ package com.samodeika.server.service;
 import com.samodeika.shared.model.Hello;
 import com.samodeika.shared.service.HelloService;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Path("/hellos")
 public class HelloServiceImpl implements HelloService {
 
     Map<String, Hello> database;
@@ -15,17 +19,25 @@ public class HelloServiceImpl implements HelloService {
         database = new HashMap<String, Hello>();
         Hello hello1 = new Hello("1", "ronan");
         Hello hello2 = new Hello("2", "john");
+        Hello hello3 = new Hello("3", "pesho");
 
         database.put(hello1.getId(), hello1);
         database.put(hello2.getId(), hello2);
+        database.put(hello3.getId(), hello3);
     }
 
+    @GET
+    @Produces("application/json")
     @Override
     public List<Hello> getHellos() {
-        List<Hello> list = new ArrayList<>(database.values());
+        List<Hello> list = new ArrayList<>();
+        list.addAll(database.values());
         return list;
     }
 
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
     @Override
     public List<Hello> getHellos(String id) {
         List<Hello> list = new ArrayList<>();
